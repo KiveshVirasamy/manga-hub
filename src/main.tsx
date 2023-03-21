b169976ebabdc6994879e1c819635dab387ea771
KiveshVirasamy <kvirasamy@jhb.dvt.co.za>
2023-03-21 14:20:26 +0200

    feat: :sparkles: Tried to implement hosting

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,28 @@ import { MangaCardContainer } from "./routes/mangaCard";
 import { MangaPage } from "./routes/mangaPages";
 import { MangaReader } from "./routes/mangaReading";
 
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAg1OuWAwhx7I7vmpCDh-aH9Kv-lKii6lU",
+  authDomain: "manga-hub-a2938.firebaseapp.com",
+  projectId: "manga-hub-a2938",
+  storageBucket: "manga-hub-a2938.appspot.com",
+  messagingSenderId: "338510297610",
+  appId: "1:338510297610:web:f6be27a097b2ce8cd0fdf3",
+  measurementId: "G-55FRGLKBJB",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
